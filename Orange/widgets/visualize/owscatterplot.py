jkbpvsc/orange3

commit 17425fd158e4e1334b5bc4d6d63747d4d2f26d93

Add icons to Scatter plot, Mosaic, Sieve and Model map

Orange/widgets/visualize/owscatterplot.py
@@ -20,8 +20,9 @@ from Orange.widgets.widget import OWWidget, Default, AttributeList
 class OWScatterPlot(OWWidget):
     name = 'Scatter plot'
     description = 'Scatter plot visualization'
+    icon = "icons/ScatterPlot.svg"
 
-    inputs = [("Data", Table, "set_data", Default),
+inputs = [("Data", Table, "set_data", Default),
               ("Data Subset", Table, "set_subset_data"),
               ("Features", AttributeList, "set_shown_attributes")]
 

Orange/widgets/visualize/icons/ModelMap.svg
@@ -1,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 16.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="48px" height="48px" viewBox="0 0 48 48" enable-background="new 0 0 48 48" xml:space="preserve">
+<g>
+	<rect x="13" y="13" fill="#333333" width="6" height="6"/>
+	<rect x="38" y="14" fill="#333333" width="6" height="6"/>
+	<rect x="24" y="35" fill="#333333" width="6" height="6"/>
+	<g>
+		<circle fill="#989898" cx="38.5" cy="9.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="34.5" cy="14.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="34.5" cy="20.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="39.5" cy="23.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="30.5" cy="10.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="9.5" cy="13.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="6.5" cy="18.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="11.5" cy="21.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="17.5" cy="23.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="12.5" cy="27.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="20.5" cy="36.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="24.5" cy="31.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="31.5" cy="31.501" r="2.5"/>
+	</g>
+	<g>
+		<circle fill="#989898" cx="33.5" cy="37.501" r="2.5"/>
+	</g>
+</g>
+</svg>

Orange/widgets/visualize/icons/MosaicDisplay.svg
@@ -1,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 16.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="48px" height="48px" viewBox="0 0 48 48" enable-background="new 0 0 48 48" xml:space="preserve">
+<rect x="7" y="19" fill="#333333" width="25" height="22"/>
+<rect x="34" y="27" fill="#333333" width="7" height="9"/>
+<rect x="34" y="7" fill="#333333" width="7" height="18"/>
+<rect x="34" y="38" fill="#333333" width="7" height="3"/>
+<rect x="7" y="7" fill="#333333" width="25" height="10"/>
+</svg>

Orange/widgets/visualize/icons/ScatterPlot.svg
@@ -1,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 16.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="48px" height="48px" viewBox="0 0 48 48" enable-background="new 0 0 48 48" xml:space="preserve">
+<g>
+	<circle fill="#333333" cx="29" cy="27.001" r="2.999"/>
+</g>
+<g>
+	<circle fill="#333333" cx="29" cy="17.003" r="2.999"/>
+</g>
+<g>
+	<circle fill="#333333" cx="18" cy="29.002" r="2.999"/>
+</g>
+<g>
+	<circle fill="#333333" cx="23" cy="34.002" r="2.999"/>
+</g>
+<g>
+	<circle fill="#333333" cx="13" cy="35.002" r="2.999"/>
+</g>
+<g>
+	<circle fill="#333333" cx="21" cy="21.002" r="2.999"/>
+</g>
+<g>
+	<circle fill="#333333" cx="38" cy="12.002" r="2.999"/>
+</g>
+<g>
+	<circle fill="#333333" cx="37" cy="21.003" r="2.999"/>
+</g>
+<polygon fill="#B2B2B2" points="8,40 8,6 6,6 6,40 6,40 6,42 6,42 8,42 42,42 42,40 "/>
+</svg>

Orange/widgets/visualize/icons/SieveDiagram.svg
@@ -1,0 +1,145 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 16.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="48px" height="48px" viewBox="0 0 48 48" enable-background="new 0 0 48 48" xml:space="preserve">
+<g>
+	<rect x="17" y="38" fill="#666666" width="4" height="3"/>
+	<rect x="17" y="33" fill="#666666" width="4" height="4"/>
+	<rect x="7" y="38" fill="#666666" width="4" height="3"/>
+	<rect x="17" y="28" fill="#666666" width="4" height="4"/>
+	<rect x="12" y="38" fill="#666666" width="4" height="3"/>
+	<rect x="12" y="28" fill="#666666" width="4" height="4"/>
+	<rect x="7" y="28" fill="#666666" width="4" height="4"/>
+	<rect x="12" y="33" fill="#666666" width="4" height="4"/>
+	<rect x="7" y="33" fill="#666666" width="4" height="4"/>
+	<rect x="27" y="12" fill="#333333" width="4" height="4"/>
+	<rect x="22" y="12" fill="#333333" width="4" height="4"/>
+	<rect x="32" y="12" fill="#333333" width="4" height="4"/>
+	<rect x="27" y="17" fill="#333333" width="4" height="4"/>
+	<rect x="22" y="17" fill="#333333" width="4" height="4"/>
+	<rect x="37" y="17" fill="#333333" width="4" height="4"/>
+	<rect x="37" y="12" fill="#333333" width="4" height="4"/>
+	<rect x="32" y="17" fill="#333333" width="4" height="4"/>
+	<rect x="22" y="28" fill="#666666" width="4" height="4"/>
+	<rect x="27" y="38" fill="#666666" width="4" height="3"/>
+	<rect x="22" y="33" fill="#666666" width="4" height="4"/>
+	<rect x="37" y="33" fill="#666666" width="4" height="4"/>
+	<rect x="22" y="38" fill="#666666" width="4" height="3"/>
+	<rect x="37" y="38" fill="#666666" width="4" height="3"/>
+	<rect x="32" y="38" fill="#666666" width="4" height="3"/>
+	<rect x="27" y="33" fill="#666666" width="4" height="4"/>
+	<rect x="27" y="28" fill="#666666" width="4" height="4"/>
+	<rect x="32" y="33" fill="#666666" width="4" height="4"/>
+	<rect x="32" y="28" fill="#666666" width="4" height="4"/>
+	<rect x="27" y="23" fill="#666666" width="4" height="4"/>
+	<rect x="22" y="23" fill="#666666" width="4" height="4"/>
+	<rect x="32" y="23" fill="#666666" width="4" height="4"/>
+	<rect x="37" y="28" fill="#666666" width="4" height="4"/>
+	<rect x="37" y="23" fill="#666666" width="4" height="4"/>
+	<rect x="27" y="7" fill="#333333" width="4" height="3"/>
+	<rect x="22" y="7" fill="#333333" width="4" height="3"/>
+	<rect x="37" y="7" fill="#333333" width="4" height="3"/>
+	<rect x="32" y="7" fill="#333333" width="4" height="3"/>
+	<rect x="7" y="22" fill="#333333" width="4" height="4"/>
+	<rect x="17" y="22" fill="#333333" width="4" height="4"/>
+	<rect x="12" y="22" fill="#333333" width="4" height="4"/>
+	<rect x="17" y="17" fill="#333333" width="4" height="4"/>
+	<rect x="17" y="12" fill="#333333" width="4" height="4"/>
+	<rect x="7" y="17" fill="#333333" width="4" height="4"/>
+	<rect x="12" y="17" fill="#333333" width="4" height="4"/>
+	<rect x="17" y="7" fill="#333333" width="4" height="4"/>
+	<rect x="12" y="7" fill="#333333" width="4" height="4"/>
+	<rect x="7" y="7" fill="#333333" width="4" height="4"/>
+	<rect x="7" y="12" fill="#333333" width="4" height="4"/>
+	<rect x="12" y="12" fill="#333333" width="4" height="4"/>
+	<rect x="11" y="28" fill="#989898" width="1" height="4"/>
+	<rect x="11" y="33" fill="#989898" width="1" height="4"/>
+	<rect x="11" y="38" fill="#989898" width="1" height="3"/>
+	<rect x="16" y="33" fill="#989898" width="1" height="4"/>
+	<rect x="16" y="38" fill="#989898" width="1" height="3"/>
+	<rect x="16" y="28" fill="#989898" width="1" height="4"/>
+	<rect x="11" y="17" fill="#666666" width="1" height="4"/>
+	<rect x="11" y="7" fill="#666666" width="1" height="4"/>
+	<rect x="11" y="22" fill="#666666" width="1" height="4"/>
+	<rect x="11" y="12" fill="#666666" width="1" height="4"/>
+	<rect x="16" y="7" fill="#666666" width="1" height="4"/>
+	<rect x="16" y="12" fill="#666666" width="1" height="4"/>
+	<rect x="16" y="22" fill="#666666" width="1" height="4"/>
+	<rect x="16" y="17" fill="#666666" width="1" height="4"/>
+	<rect x="26" y="7" fill="#666666" width="1" height="3"/>
+	<rect x="31" y="7" fill="#666666" width="1" height="3"/>
+	<rect x="36" y="7" fill="#666666" width="1" height="3"/>
+	<rect x="26" y="12" fill="#666666" width="1" height="4"/>
+	<rect x="26" y="17" fill="#666666" width="1" height="4"/>
+	<rect x="31" y="12" fill="#666666" width="1" height="4"/>
+	<rect x="31" y="17" fill="#666666" width="1" height="4"/>
+	<rect x="36" y="17" fill="#666666" width="1" height="4"/>
+	<rect x="36" y="12" fill="#666666" width="1" height="4"/>
+	<rect x="26" y="23" fill="#989898" width="1" height="4"/>
+	<rect x="26" y="33" fill="#989898" width="1" height="4"/>
+	<rect x="26" y="28" fill="#989898" width="1" height="4"/>
+	<rect x="26" y="38" fill="#989898" width="1" height="3"/>
+	<rect x="31" y="28" fill="#989898" width="1" height="4"/>
+	<rect x="31" y="38" fill="#989898" width="1" height="3"/>
+	<rect x="31" y="23" fill="#989898" width="1" height="4"/>
+	<rect x="31" y="33" fill="#989898" width="1" height="4"/>
+	<rect x="36" y="33" fill="#989898" width="1" height="4"/>
+	<rect x="36" y="38" fill="#989898" width="1" height="3"/>
+	<rect x="36" y="28" fill="#989898" width="1" height="4"/>
+	<rect x="36" y="23" fill="#989898" width="1" height="4"/>
+	<rect x="12" y="37" fill="#989898" width="4" height="1"/>
+	<rect x="17" y="37" fill="#989898" width="4" height="1"/>
+	<rect x="7" y="37" fill="#989898" width="4" height="1"/>
+	<rect x="11" y="37" fill="#989898" width="1" height="1"/>
+	<rect x="16" y="37" fill="#989898" width="1" height="1"/>
+	<rect x="12" y="32" fill="#989898" width="4" height="1"/>
+	<rect x="17" y="32" fill="#989898" width="4" height="1"/>
+	<rect x="7" y="32" fill="#989898" width="4" height="1"/>
+	<rect x="11" y="32" fill="#989898" width="1" height="1"/>
+	<rect x="16" y="32" fill="#989898" width="1" height="1"/>
+	<rect x="27" y="37" fill="#989898" width="4" height="1"/>
+	<rect x="37" y="37" fill="#989898" width="4" height="1"/>
+	<rect x="32" y="37" fill="#989898" width="4" height="1"/>
+	<rect x="22" y="37" fill="#989898" width="4" height="1"/>
+	<rect x="26" y="37" fill="#989898" width="1" height="1"/>
+	<rect x="31" y="37" fill="#989898" width="1" height="1"/>
+	<rect x="36" y="37" fill="#989898" width="1" height="1"/>
+	<rect x="37" y="32" fill="#989898" width="4" height="1"/>
+	<rect x="32" y="32" fill="#989898" width="4" height="1"/>
+	<rect x="22" y="32" fill="#989898" width="4" height="1"/>
+	<rect x="27" y="32" fill="#989898" width="4" height="1"/>
+	<rect x="26" y="32" fill="#989898" width="1" height="1"/>
+	<rect x="31" y="32" fill="#989898" width="1" height="1"/>
+	<rect x="36" y="32" fill="#989898" width="1" height="1"/>
+	<rect x="32" y="27" fill="#989898" width="4" height="1"/>
+	<rect x="22" y="27" fill="#989898" width="4" height="1"/>
+	<rect x="37" y="27" fill="#989898" width="4" height="1"/>
+	<rect x="27" y="27" fill="#989898" width="4" height="1"/>
+	<rect x="26" y="27" fill="#989898" width="1" height="1"/>
+	<rect x="31" y="27" fill="#989898" width="1" height="1"/>
+	<rect x="36" y="27" fill="#989898" width="1" height="1"/>
+	<rect x="32" y="16" fill="#666666" width="4" height="1"/>
+	<rect x="22" y="16" fill="#666666" width="4" height="1"/>
+	<rect x="27" y="16" fill="#666666" width="4" height="1"/>
+	<rect x="37" y="16" fill="#666666" width="4" height="1"/>
+	<rect x="26" y="16" fill="#666666" width="1" height="1"/>
+	<rect x="31" y="16" fill="#666666" width="1" height="1"/>
+	<rect x="36" y="16" fill="#666666" width="1" height="1"/>
+	<rect x="12" y="21" fill="#666666" width="4" height="1"/>
+	<rect x="17" y="21" fill="#666666" width="4" height="1"/>
+	<rect x="7" y="21" fill="#666666" width="4" height="1"/>
+	<rect x="11" y="21" fill="#666666" width="1" height="1"/>
+	<rect x="16" y="21" fill="#666666" width="1" height="1"/>
+	<rect x="17" y="16" fill="#666666" width="4" height="1"/>
+	<rect x="7" y="16" fill="#666666" width="4" height="1"/>
+	<rect x="12" y="16" fill="#666666" width="4" height="1"/>
+	<rect x="11" y="16" fill="#666666" width="1" height="1"/>
+	<rect x="16" y="16" fill="#666666" width="1" height="1"/>
+	<rect x="12" y="11" fill="#666666" width="4" height="1"/>
+	<rect x="17" y="11" fill="#666666" width="4" height="1"/>
+	<rect x="7" y="11" fill="#666666" width="4" height="1"/>
+	<rect x="11" y="11" fill="#666666" width="1" height="1"/>
+	<rect x="16" y="11" fill="#666666" width="1" height="1"/>
+</g>
+</svg>

Orange/widgets/visualize/owmosaic.py
@@ -103,6 +103,10 @@ class OWMosaicDisplay(OWWidget):
     <priority>4100</priority>
     """
     name = "Mosaic Display"
+    description = "Shows mosaic displays"
+    long_description = """Shows mosaic displays"""
+    icon = "icons/MosaicDisplay.svg"
+
     inputs = [("Data", Table, "setData", Default),
               ("Data Subset", Table, "setSubsetData")]
     outputs = [("Selected Data", Table), ("Learner", Fitter)]

Orange/widgets/visualize/owmpr.py
@@ -49,6 +49,7 @@ def p_index(ct):
 class OWMPR(OWWidget):
     name = 'ModelMap Projection Rank'
     description = 'Ranking projections by estimating projection quality'
+    icon = "icons/ModelMap.svg"
 
     inputs = [('Data', Table, 'set_data', Default)]
     outputs = [('Features', AttributeList)]

Orange/widgets/visualize/owsieve.py
@@ -27,6 +27,8 @@ class OWSieveDiagram(OWWidget):
     <priority>4200</priority>
     """
     name = "Sieve Diagram"
+    icon = "icons/SieveDiagram.svg"
+
     inputs = [("Data", Table, "setData", Default), ("Features", AttributeList, "setShownAttributes")]
     outputs = []
 

commit b49aada84cc24113d0d74411ebb82db196f8484d

Fix scatterplot bug introduced in 17425fd1.

Orange/widgets/visualize/owscatterplot.py
@@ -22,7 +22,7 @@ class OWScatterPlot(OWWidget):
     description = 'Scatter plot visualization'
     icon = "icons/ScatterPlot.svg"
 
-inputs = [("Data", Table, "set_data", Default),
+    inputs = [("Data", Table, "set_data", Default),
               ("Data Subset", Table, "set_subset_data"),
               ("Features", AttributeList, "set_shown_attributes")]
 