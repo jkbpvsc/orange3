jkbpvsc/orange3

commit 35bb9bbb40cfde51de3b133f473986f3897804b5

Predictions: move predictions to metas

Orange/widgets/evaluate/owpredictions.py
@@ -162,55 +162,10 @@ class OWPredictions(widget.OWWidget):
         predictor = next(iter(self.predictors.values())).predictor
         class_var = predictor.domain.class_var
         classification = class_var and class_var.is_discrete
-
-        newattrs = []
-        newcolumns = []
         slots = list(self.predictors.values())
 
-        if classification:
-            if self.show_predictions:
-                mc = [DiscreteVariable(name=p.name, values=class_var.values)
-                      for p in slots]
-                newattrs.extend(mc)
-                newcolumns.extend(p.results[0].reshape((-1, 1))
-                                  for p in slots)
-
-            if self.show_probabilities:
-                for p in slots:
-                    m = [ContinuousVariable(name="%s(%s)" % (p.name, value))
-                         for value in class_var.values]
-                    newattrs.extend(m)
-                newcolumns.extend(p.results[1] for p in slots)
-
-        else:
-            # regression
-            mc = [ContinuousVariable(name=p.name)
-                  for p in self.predictors.values()]
-            newattrs.extend(mc)
-            newcolumns.extend(p.results[0].reshape((-1, 1))
-                              for p in slots)
-
-        if self.show_attrs:
-            X = [self.data.X]
-            attrs = list(self.data.domain.attributes) + newattrs
-        else:
-            X = []
-            attrs = newattrs
-        domain = Orange.data.Domain(attrs, self.data.domain.class_var,
-                                    metas=self.data.domain.metas)
-
-        if newcolumns:
-            X.extend(numpy.atleast_2d(cols) for cols in newcolumns)
-        if X:
-            X = numpy.hstack(tuple(X))
-        else:
-            X = numpy.zeros((len(self.data), 0))
-
-        predictions = Orange.data.Table.from_numpy(
-            domain, X, self.data.Y, metas=self.data.metas)
-        predictions.name = self.data.name
-
         results = None
+        predictions = None
         if self.data.domain.class_var == class_var:
             N = len(self.data)
             results = Orange.evaluation.Results(self.data, store_data=True)
@@ -223,6 +178,11 @@ class OWPredictions(widget.OWWidget):
                 results.probabilities = numpy.array(
                     [p.results[1] for p in slots])
             results.learner_names = [pname(p.predictor) for p in slots]
+
+            predictions = results.get_augmented_data(results.learner_names,
+                                                     include_attrs=self.show_attrs,
+                                                     include_predictions=self.show_predictions,
+                                                     include_probabilities=self.show_probabilities)
 
         self.send("Predictions", predictions)
         self.send("Evaluation Results", results)

Orange/evaluation/testing.py
@@ -202,13 +202,16 @@ class Results:
 
         return results
 
-    def get_augmented_data(self, model_names):
+    def get_augmented_data(self, model_names, include_attrs=True, include_predictions=True, include_probabilities=True):
         """
         Return the data, augmented with predictions, probabilities (if the task is classification) and folds info.
         Predictions, probabilities and folds are inserted as meta attributes.
 
         Args:
             model_names (list): A list of strings containing learners' names.
+            include_attrs (bool): Flag that tells whether to include original attributes.
+            include_predictions (bool): Flag that tells whether to include predictions.
+            include_probabilities (bool): Flag that tells whether to include probabilities.
 
         Returns:
             Orange.data.Table: Data augmented with predictions, (probabilities) and (fold).
@@ -225,19 +228,21 @@ class Results:
 
         if classification:
             # predictions
-            new_meta_attr.extend(DiscreteVariable(name=name, values=class_var.values)
-                                 for name in model_names)
-            new_meta_vals = np.hstack((new_meta_vals, self.predicted.T))
+            if include_predictions:
+                new_meta_attr.extend(DiscreteVariable(name=name, values=class_var.values)
+                                     for name in model_names)
+                new_meta_vals = np.hstack((new_meta_vals, self.predicted.T))
 
             # probabilities
-            for name in model_names:
-                new_meta_attr.extend(ContinuousVariable(name="%s (%s)" % (name, value))
-                                     for value in class_var.values)
+            if include_probabilities:
+                for name in model_names:
+                    new_meta_attr.extend(ContinuousVariable(name="%s (%s)" % (name, value))
+                                         for value in class_var.values)
 
-            for i in self.probabilities:
-                new_meta_vals = np.hstack((new_meta_vals, i))
+                for i in self.probabilities:
+                    new_meta_vals = np.hstack((new_meta_vals, i))
 
-        else:
+        elif include_predictions:
             # regression
             new_meta_attr.extend(ContinuousVariable(name=name)
                                  for name in model_names)
@@ -255,8 +260,11 @@ class Results:
         new_meta_attr = list(data.domain.metas) + new_meta_attr
         new_meta_vals = np.hstack((data.metas, new_meta_vals))
 
-        domain = Domain(data.domain.attributes, data.domain.class_vars, metas=new_meta_attr)
-        predictions = Table.from_numpy(domain, data.X, data.Y, metas=new_meta_vals)
+        X = data.X if include_attrs else np.empty((len(data), 0))
+        attrs = data.domain.attributes if include_attrs else []
+
+        domain = Domain(attrs, data.domain.class_vars, metas=new_meta_attr)
+        predictions = Table.from_numpy(domain, X, data.Y, metas=new_meta_vals)
         predictions.name = data.name
         return predictions
 

commit 2c33f15bac44e9c756e6d7465bec45610df4907a

OWPredictions: Output predictions when no class_var is present
Partially revert 35bb9bbb40cfde51de3b133f473986f3897804b5, which caused
the "Predictions" output to no longer be sent if the input dataset did
not contain the actual/true class column.

Orange/widgets/evaluate/owpredictions.py
@@ -164,10 +164,55 @@ class OWPredictions(widget.OWWidget):
         predictor = next(iter(self.predictors.values())).predictor
         class_var = predictor.domain.class_var
         classification = class_var and class_var.is_discrete
+
+        newattrs = []
+        newcolumns = []
         slots = list(self.predictors.values())
 
+        if classification:
+            if self.show_predictions:
+                mc = [DiscreteVariable(name=p.name, values=class_var.values)
+                      for p in slots]
+                newattrs.extend(mc)
+                newcolumns.extend(p.results[0].reshape((-1, 1))
+                                  for p in slots)
+
+            if self.show_probabilities:
+                for p in slots:
+                    m = [ContinuousVariable(name="%s(%s)" % (p.name, value))
+                         for value in class_var.values]
+                    newattrs.extend(m)
+                newcolumns.extend(p.results[1] for p in slots)
+
+        else:
+            # regression
+            mc = [ContinuousVariable(name=p.name)
+                  for p in self.predictors.values()]
+            newattrs.extend(mc)
+            newcolumns.extend(p.results[0].reshape((-1, 1))
+                              for p in slots)
+
+        if self.show_attrs:
+            X = [self.data.X]
+            attrs = list(self.data.domain.attributes) + newattrs
+        else:
+            X = []
+            attrs = newattrs
+        domain = Orange.data.Domain(attrs, self.data.domain.class_var,
+                                    metas=self.data.domain.metas)
+
+        if newcolumns:
+            X.extend(numpy.atleast_2d(cols) for cols in newcolumns)
+        if X:
+            X = numpy.hstack(tuple(X))
+        else:
+            X = numpy.zeros((len(self.data), 0))
+
+        predictions = Orange.data.Table.from_numpy(
+            domain, X, self.data.Y, metas=self.data.metas)
+        predictions.name = self.data.name
+
         results = None
-        predictions = None
         if self.data.domain.class_var == class_var:
             N = len(self.data)
             results = Orange.evaluation.Results(self.data, store_data=True)
@@ -180,11 +225,6 @@ class OWPredictions(widget.OWWidget):
                 results.probabilities = numpy.array(
                     [p.results[1] for p in slots])
             results.learner_names = [pname(p.predictor) for p in slots]
-
-            predictions = results.get_augmented_data(results.learner_names,
-                                                     include_attrs=self.show_attrs,
-                                                     include_predictions=self.show_predictions,
-                                                     include_probabilities=self.show_probabilities)
 
         self.send("Predictions", predictions)
         self.send("Evaluation Results", results)