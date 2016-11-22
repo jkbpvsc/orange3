jkbpvsc/orange3

commit 959f696b68e68ab67ea44b49dd1070c217ee62b1

SharedComputeValue: Transformation class uses from_table instead of its partial reimplementation.

Orange/preprocess/transformation.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from Orange.data import Instance, Table
+from Orange.data import Instance, Table, Domain
 
 
 class Transformation:
@@ -22,23 +22,12 @@ class Transformation:
         from the data and passing it to the `transform` method.
         """
         inst = isinstance(data, Instance)
-        if self._last_domain != data.domain:
-            try:
-                self.attr_index = data.domain.index(self.variable)
-            except ValueError:
-                if self.variable.compute_value is None:
-                    raise ValueError("{} is not in domain".
-                                     format(self.variable.name))
-                self.attr_index = None
-            self._last_domain = data.domain
-        if self.attr_index is None:
-            data = self.variable.compute_value(data)
-        elif inst:
-            data = np.array([float(data[self.attr_index])])
-        else:
-            data = data.get_column_view(self.attr_index)[0]
-        transformed = self.transform(data)
-        if inst and isinstance(transformed, np.ndarray) and transformed.shape:
+        if inst:
+            data = Table(data.domain, [data])
+        domain = Domain([self.variable])
+        data = Table.from_table(domain, data)
+        transformed = self.transform(data.X.squeeze(axis=1))
+        if inst:
             transformed = transformed[0]
         return transformed
 

commit 3687ecfde14fdf95d4d515d44b4888234d420c57

Fix transformation for non primitive variables
Fix a 'TypeError: variables must be primitive' when a transformation
is defined for a `StringVariable` (as is the case in 'Edit Domain'
widget).

This was broken since 959f696b68e68ab67ea44b49dd1070c217ee62b1

Orange/preprocess/transformation.py
@@ -24,9 +24,13 @@ class Transformation:
         inst = isinstance(data, Instance)
         if inst:
             data = Table(data.domain, [data])
-        domain = Domain([self.variable])
-        data = Table.from_table(domain, data)
-        transformed = self.transform(data.X if sp.issparse(data.X) else data.X.squeeze(axis=1))
+        if self.variable.is_primitive():
+            domain = Domain([self.variable])
+            data = Table.from_table(domain, data).X
+        else:
+            domain = Domain([], [], metas=[self.variable])
+            data = Table.from_table(domain, data).metas
+        transformed = self.transform(data if sp.issparse(data) else data.squeeze(axis=1))
         if inst:
             transformed = transformed[0]
         return transformed

Orange/tests/test_transformation.py
@@ -1,0 +1,27 @@
+import unittest
+import numpy as np
+
+from Orange.data import Table, Domain, DiscreteVariable, ContinuousVariable, \
+    StringVariable
+from Orange.preprocess.transformation import Identity
+
+
+class TestTransformation(unittest.TestCase):
+    def test_identity(self):
+        domain = Domain([ContinuousVariable("X")],
+                        [DiscreteVariable("C", values=["0", "1", "2"])],
+                        [StringVariable("S")])
+        X = np.random.normal(size=(4, 1))
+        Y = np.random.randint(3, size=(4, 1))
+        M = np.array(["A", "B", "C", "D"], dtype=object).reshape(-1, 1)
+
+        D = Table.from_numpy(domain, X, Y, metas=M)
+        X1 = domain[0].copy(compute_value=Identity(domain[0]))
+        Y1 = domain[1].copy(compute_value=Identity(domain[1]))
+        S1 = domain.metas[0].copy(compute_value=Identity(domain.metas[0]))
+        domain_1 = Domain([X1], [Y1], [S1])
+        D1 = Table.from_table(domain_1, D)
+
+        np.testing.assert_equal(D1.X, D.X)
+        np.testing.assert_equal(D1.Y, D.Y)
+        np.testing.assert_equal(D1.metas, D.metas)